960882752ca698ffde2eda92b9acd3668ca3ff80
BR3XALITY <marioplays254@gmail.com>
2026-01-31 23:01:16 -0800

    add 2 new modes to the HBOMB Gun

--- lua/weapons/hbombgun.lua
+++ lua/weapons/hbombgun.lua
@@ -1,7 +1,7 @@
 SWEP.PrintName = "HBOMB Gun"
 SWEP.Author = "BR3XALITY"
 SWEP.Purpose = "shoots helecopter bombs"
-SWEP.Instructions = "LEFT CLICK: Throw hbomb | RIGHT CLICK: Toggle rapid fire"
+SWEP.Instructions = "LEFT CLICK: Throw hbomb | RIGHT CLICK: Cycle fire mode"
 SWEP.Category = "The Fun Guns"
 SWEP.Spawnable = true
 SWEP.AdminOnly = false
@@ -21,7 +21,7 @@ SWEP.Primary.NumberofShots = 1
 SWEP.Primary.Automatic = true
 SWEP.Primary.Recoil = .2
 SWEP.Primary.Delay = 0.5
-SWEP.Primary.Force = 10
+SWEP.Primary.Force = 10 -- base force for normal modes
 
 SWEP.Secondary.ClipSize = -1
 SWEP.Secondary.DefaultClip = -1
@@ -45,12 +45,22 @@ SWEP.FiresUnderwater = false
 SWEP.ReloadSound = "sound/epicreload.wav"
 SWEP.CSMuzzleFlashes = true
 
-SWEP.RapidFireMode = false
+-- Mode system:
+-- 1 = Normal
+-- 2 = Rapid
+-- 3 = Normal - Hard Force
+-- 4 = Rapid - Hard Force
 SWEP.NormalDelay = 0.5
 SWEP.RapidDelay = 0.05
+SWEP.HardForceMultiplier = 10 -- multiplies base Primary.Force for hard modes (10 * 3 = 30)
+SWEP.BaseVelocityNormal = 1500
+SWEP.BaseVelocityRapid = 1200
+SWEP.HardVelocityNormal = 3000
+SWEP.HardVelocityRapid = 2400
 
 function SWEP:SetupDataTables()
-    self:NetworkVar("Bool", 0, "RapidFireMode")
+    -- store an int for the current fire mode (1..4)
+    self:NetworkVar("Int", 0, "FireMode")
 end
 
 function SWEP:Initialize()
@@ -59,26 +69,43 @@ function SWEP:Initialize()
     util.PrecacheSound(RapidFireSound)
     self:SetWeaponHoldType(self.HoldType)
     if SERVER then
-        self:SetRapidFireMode(false)
+        self:SetFireMode(1) -- start in mode 1
     end
     self.Primary.Delay = self.NormalDelay
 end
 
+local function modeIsRapid(mode)
+    return mode == 2 or mode == 4
+end
+
+local function modeIsHard(mode)
+    return mode == 3 or mode == 4
+end
+
 function SWEP:PrimaryAttack()
     if (!self:CanPrimaryAttack()) then return end
+    local mode = self:GetFireMode() or 1
+    local isRapid = modeIsRapid(mode)
+    local isHard = modeIsHard(mode)
+
+    -- ensure proper delay on firing
+    self.Primary.Delay = isRapid and self.RapidDelay or self.NormalDelay
     self:SetNextPrimaryFire(CurTime() + self.Primary.Delay)
     self:SendWeaponAnim(ACT_VM_PRIMARYATTACK)
     self:EmitSound(ShootSound)
     if IsValid(self.Owner) then
         self.Owner:SetAnimation(PLAYER_ATTACK1)
     end
-    local recoilMult = (self:GetRapidFireMode() and 0.5) or 1
+
+    local recoilMult = (isRapid and 0.5) or 1
     local rnda = self.Primary.Recoil * -1 * recoilMult
     local rndb = self.Primary.Recoil * math.random(-1, 1) * recoilMult
     if IsValid(self.Owner) then
         self.Owner:ViewPunch(Angle(rnda, rndb, rnda))
     end
+
     self:TakePrimaryAmmo(self.Primary.TakeAmmo)
+
     if (!SERVER) then return end
     local owner = self.Owner
     local spawnPos = owner:GetShootPos()
@@ -87,6 +114,7 @@ function SWEP:PrimaryAttack()
                          math.Rand(-self.Primary.Spread, self.Primary.Spread),
                          0)
     aimVec = (aimVec + spread):GetNormalized()
+
     local grenade = ents.Create("grenade_helicopter")
     if (!IsValid(grenade)) then return end
     grenade:SetPos(spawnPos + aimVec * 16)
@@ -94,13 +122,28 @@ function SWEP:PrimaryAttack()
     grenade:SetOwner(owner)
     grenade:Spawn()
     grenade:Activate()
-    local velocityMult = (self:GetRapidFireMode() and 1200) or 1500
+
+    -- choose velocity based on mode (hard modes launch faster)
+    local velocityMult
+    if isHard then
+        velocityMult = isRapid and self.HardVelocityRapid or self.HardVelocityNormal
+    else
+        velocityMult = isRapid and self.BaseVelocityRapid or self.BaseVelocityNormal
+    end
+
     local velocity = aimVec * velocityMult
     local phys = grenade:GetPhysicsObject()
     if (IsValid(phys)) then
         phys:SetVelocity(velocity)
         phys:AddAngleVelocity(Vector(math.random(-500, 500), math.random(-500, 500), math.random(-500, 500)))
+        -- apply harder force impulse in hard modes to affect collisions more strongly
+        if isHard then
+            local baseForce = self.Primary.Force or 10
+            phys:ApplyForceCenter(aimVec * (baseForce * self.HardForceMultiplier))
+        end
     end
+
+    -- cleanup
     timer.Simple(30, function()
         if (IsValid(grenade)) then
             grenade:Remove()
@@ -111,22 +154,27 @@ end
 function SWEP:SecondaryAttack()
     if (!self:CanSecondaryAttack()) then return end
     if SERVER then
-        self:SetRapidFireMode(not self:GetRapidFireMode())
-        if (self:GetRapidFireMode()) then
-            self.Primary.Delay = self.RapidDelay
-            if IsValid(self.Owner) then
-                self.Owner:ChatPrint("RAPID FIRE MODE ENABLED (0.05s delay)")
-            end
-        else
-            self.Primary.Delay = self.NormalDelay
-            if IsValid(self.Owner) then
-                self.Owner:ChatPrint("NORMAL FIRE MODE (0.5s delay)")
-            end
+        local mode = self:GetFireMode() or 1
+        mode = mode + 1
+        if mode > 4 then mode = 1 end
+        self:SetFireMode(mode)
+
+        -- set the delay instantly for feedback / consistency
+        local isRapid = modeIsRapid(mode)
+        self.Primary.Delay = isRapid and self.RapidDelay or self.NormalDelay
+
+        if IsValid(self.Owner) then
+            local label
+            if mode == 1 then label = "NORMAL FIRE (MODE 1)"
+            elseif mode == 2 then label = "RAPID FIRE (MODE 2)"
+            elseif mode == 3 then label = "NORMAL - HARD FORCE (MODE 3)"
+            elseif mode == 4 then label = "RAPID - HARD FORCE (MODE 4)" end
+            self.Owner:ChatPrint(label)
         end
     end
     self:EmitSound(RapidFireSound)
     self:SendWeaponAnim(ACT_VM_SECONDARYATTACK)
-    self:SetNextSecondaryFire(CurTime() + 0.5)
+    self:SetNextSecondaryFire(CurTime() + 0.3)
 end
 
 function SWEP:CanSecondaryAttack()
@@ -140,18 +188,39 @@ end
 
 function SWEP:DrawHUD()
     if not IsValid(self:GetOwner()) then return end
-    local modeText = (self:GetRapidFireMode() and "RAPID FIRE") or "NORMAL FIRE"
-    local color = (self:GetRapidFireMode() and Color(255, 50, 50, 255)) or Color(50, 255, 50, 255)
+    local mode = self:GetFireMode() or 1
+    local modeText = "MODE " .. tostring(mode)
+    local delay = 0.5 -- default fallback
+
+    -- Determine delay based on mode
+    if mode == 1 then
+        modeText = "NORMAL"
+        delay = self.NormalDelay
+    elseif mode == 2 then
+        modeText = "RAPID"
+        delay = self.RapidDelay
+    elseif mode == 3 then
+        modeText = "NORMAL - HARD"
+        delay = self.NormalDelay
+    elseif mode == 4 then
+        modeText = "RAPID - HARD"
+        delay = self.RapidDelay
+    end
+
+    local color = (mode == 1 or mode == 3) and Color(50, 255, 50, 255) or Color(255, 50, 50, 255)
+
     surface.SetFont("Default")
     surface.SetTextColor(color)
     surface.SetTextPos(ScrW() / 2 + 20, ScrH() / 2 + 20)
     surface.DrawText("MODE: " .. modeText)
     surface.SetTextPos(ScrW() / 2 + 20, ScrH() / 2 + 35)
-    surface.DrawText("DELAY: " .. self.Primary.Delay .. "s")
+    surface.DrawText("DELAY: " .. string.format("%.2f", delay) .. "s")
 end
 
 function SWEP:Think()
-    if (self:GetRapidFireMode() and self:GetNextPrimaryFire() > CurTime() - 0.1) then
+    -- keep muzzle FX chance for rapid modes like before
+    local mode = self:GetFireMode() or 1
+    if (modeIsRapid(mode) and self:GetNextPrimaryFire() > CurTime() - 0.1) then
         if (math.random(1, 3) == 1) then
             local effectdata = EffectData()
             effectdata:SetOrigin(self.Owner and self.Owner:GetShootPos() or Vector(0,0,0))
